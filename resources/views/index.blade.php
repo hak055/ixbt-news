753383c7acdaa310d1ff39e81abef160fe9c2e2b
hak055 <hak055-sahakyan@yandex.ru>
2019-06-06 12:51:22 +0300

    any

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,16 +6,14 @@
 <h1>Новостная лента</h1>
 @if($news_lists->count())
 {{ $news_lists->links() }}
-<img src="https://www.ixbt.com/img/r30/00/02/18/91/-.jpg" align="left" hspace="20">
 	@foreach($news_lists as $news)
 	<div class="list-group p-1 h-50 w-100">
 	  <a href="{{$news->link}}" class="list-group-item list-group-item-action">
 	    <div class="d-flex w-100 justify-content-between">
 	      <h5 class="mb-1">{{$news->title}}</h5>
-	      <small>Категория: {{$news->category}}/</small>	      
+	      <small>Категория: {{$news->category}}/</small>     
 	    </div>
 	    <p class="mb-1">{!! html_entity_decode($news->description) !!}</p>
-	    <p class="mb-1 h-50 w-25"></p>
 	    <small>{{$news->pubDate}}</small>
 	    <small>Автор: {{$news->author}}</small>
 	  </a>
